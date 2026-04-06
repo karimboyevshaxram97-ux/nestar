6c0965e6b246bd8d6bd573e3f9d03dabc2e208bb
KEVIN <karimboyevshaxram97@gmail.com>
2026-04-06 19:59:56 +0900

    feat: create member rezolve vs service model

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -10,7 +10,9 @@ import { DatabaseModule } from './database/database.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
+  imports: 
+   [ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
     driver: ApolloDriver,
     playground: true,
     uploads: false,
