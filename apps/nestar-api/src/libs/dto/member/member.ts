c82c5f3471e24e4c22568bccda94713f289d1023
KEVIN <karimboyevshaxram97@gmail.com>
2026-04-14 05:31:19 +0900

    feat: develop getAgents GraphQL api

--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -84,3 +84,17 @@ export class Member {
 
 }
 
+@ObjectType()
+export class TotalCounter {
+  @Field(() => Int, { nullable: true })
+  total: number;
+}
+
+@ObjectType()
+export class Members {
+  @Field(() => [Member])
+  list: Member[];
+
+  @Field(() => [TotalCounter], { nullable: true })
+  metaCounter: TotalCounter[];
+}
--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -1,3 +1,5 @@
+import { registerEnumType } from "@nestjs/graphql";
+
 export enum Message {
   SOMETHING_WENT_WRONG = 'Something went wrong!',
   NO_DATA_FOUND = 'No data found!',
@@ -19,3 +21,12 @@ export enum Message {
   SELF_SUBSCRIPTION_DENIED = 'Self subscription is denied!',
 
 }
+
+export enum Direction {
+  ASC = 1,
+  DESC = -1,
+}
+
+registerEnumType(Direction, {
+  name: 'Direction',
+});
