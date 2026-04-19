0135e84a20ab70e355b3deb626b52ac2af91342c
KEVIN <karimboyevshaxram97@gmail.com>
2026-04-19 16:58:08 +0900

    feat: develop  removePropertyByAdmin business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -100,4 +100,15 @@ public async updatePropertyByAdmin(
   return await this.propertyService.updatePropertyByAdmin(input);
 }
  
+//======================================================================
+@Roles(MemberType.ADMIN)
+@UseGuards(RolesGuard)
+@Mutation((returns) => Property)
+public async removePropertyByAdmin(
+  @Args('propertyId') input: string,
+): Promise<Property> {
+  console.log('Mutation: removePropertyByAdmin');
+  const propertyId = shapeIntoMongoObjectId(input);
+  return await this.propertyService.removePropertyByAdmin(propertyId);
+}
 }
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -281,4 +281,13 @@ public async getAllPropertiesByAdmin(input: AllPropertiesInquiry): Promise<Prope
   return result;
 }
 
+//==============================================================
+public async removePropertyByAdmin(propertyId: ObjectId): Promise<Property> {
+  const search: T = { _id: propertyId, propertyStatus: PropertyStatus.DELETE };
+  const result = await this.propertyModel.findOneAndDelete(search).exec();
+  if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+  return result;
+}
+
 }
