1ae325b0476900e4b52ecd85a8809c476cfc4db3
KEVIN <karimboyevshaxram97@gmail.com>
2026-05-28 01:35:40 +0900

    feat: develop WebSocket Chatting Server first stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -1,28 +1,78 @@
 import { Logger } from '@nestjs/common';                                        // Log yozish uchun
-import { OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'; // WebSocket dekoratorlari
-import { Server } from 'ws';                                                    // WebSocket Server tipi
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets'; // WebSocket dekoratorlari
+import { Server } from 'ws';    // WebSocket Server tipi
+import * as WebSocket from "ws";
+
+interface MessagePayload {
+  event: string;
+  text: string;
+}
+
+interface InfoPayload {
+  event: string;
+  totalClients: number;
+}
 
 @WebSocketGateway({ transports: ['websocket'], secure: false })                 // WebSocket Gateway, xavfsiz emas (dev uchun)
 export class SocketGateway implements OnGatewayInit {                          // OnGatewayInit — server ishga tushganda chaqiriladi
   private logger: Logger = new Logger('SocketEventsGateway');                  // Logger yaratadi (SocketEventsGateway nomi bilan)
   private summaryClient: number = 0;                                           // Ulangan clientlar soni (default: 0)
 
+@WebSocketServer()
+server: Server;
+
   public afterInit(server: Server) {                                            // Server ishga tushganda chaqiriladi
-    this.logger.log(`WebSocket Server Initialized total: ${this.summaryClient}`); // Server ishga tushganini log qiladi
+    this.logger.log(`WebSocket Server Initialized & total: [${this.summaryClient}]`); // Server ishga tushganini log qiladi
   }
 
   handleConnection(client: WebSocket, ...args: any[]) {                        // Yangi client ulanganda chaqiriladi
     this.summaryClient++;                                                       // Clientlar sonini +1 oshiradi
-    this.logger.log(`== Client connected total: ${this.summaryClient} ==`);    // Ulangan clientlar sonini log qiladi
+    this.logger.verbose(` Connection & total: [${this.summaryClient} ==]`);    // Ulangan clientlar sonini log qiladi
+   
+  const infoMsg: InfoPayload = {
+    event: 'Info',
+    totalClients: this.summaryClient,
+  };  
+   this.emitMessage(infoMsg);
   }
 
-  handleDisconnect(client: WebSocket) {                                        // Client uzilganda chaqiriladi
-    this.summaryClient--;                                                       // Clientlar sonini -1 kamaytiradi
-    this.logger.log(`== Client disconnected left total: ${this.summaryClient} ==`); // Qolgan clientlar sonini log qiladi
-  }
 
-  @SubscribeMessage('message')                                                  // 'message' eventini tinglaydi
-  public handleMessage(client: WebSocket, payload: any): string {              // Client xabar yuborganda chaqiriladi
-    return 'Hello world!';                                                      // Clientga 'Hello world!' qaytaradi
+
+  handleDisconnect(client: WebSocket) {
+  this.summaryClient--;
+  this.logger.verbose(`Disconnection & total [${this.summaryClient}]`);
+
+  const infoMsg: InfoPayload = {
+    event: 'info',
+    totalClients: this.summaryClient,
+  };
+  this.broadcastMessage(client, infoMsg);
   }
+  
+ @SubscribeMessage('message')
+public async handleMessage(client: WebSocket, payload: string): Promise<void> {
+  const newMessage: MessagePayload = { event: 'message', text: payload };
+  this.logger.verbose(`NEW MESSAGE: ${payload}`);
+  this.emitMessage(newMessage);
+}
+
+  
+  private broadcastMessage(sender: WebSocket, message: InfoPayload | MessagePayload ) {
+    this.server.clients.forEach((client) => {
+      if (client !== sender && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(message));
+      }
+    });
+  }
+  
+
+  private emitMessage(message: InfoPayload | MessagePayload) {
+  this.server.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
+
+
 }
